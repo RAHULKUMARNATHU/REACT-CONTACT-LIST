3c67a6e86dc0f10736b0fde25c73782a0100df59
Rahul Kumar <rahulkumars9572@gmail.com>
2022-09-14 08:25:06 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
   <React.StrictMode>
-    <App />
+    <App  />
   </React.StrictMode>
   </Provider>
 );
