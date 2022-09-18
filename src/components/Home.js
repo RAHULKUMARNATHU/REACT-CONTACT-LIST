8c43602020aec2eb8239086bfcbfa6affb8f44d4
RAHUL KUMAR <me.kumarrahul9572@gmail.com>
2022-09-18 17:20:18 +0530

    commit :: added icons to edit and delete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,68 +1,52 @@
 import "../index.css";
 import CreateContact from "./CreateContact";
-import Button from "react-bootstrap/Button";
 import { connect } from "react-redux";
 import { removeContact } from "../actions/contacts";
 
-
-function Home (props){
-
-
-  const {users} = props;
-
+function Home(props) {
+  const { users } = props;
 
   const handleDelete = (contact) => {
     props.dispatch(removeContact(contact));
-  }
-    return (
+  };
 
-        <>
-   
-        <div className="text-area">
-          <h2>Contact Lists</h2>
+  const handleEdit = () => {};
+  return (
+    <>
+      <div className="text-area">
+        <h2>Contact Lists</h2>
+      </div>
+      <div className="create-contact">
+        <CreateContact />
+      </div>
+      <div className="list-container">
+        <div className="main-container">
+          {users.map((element) => (
+            <div className="container">
+              <span>{element.name}</span>
+              <span>{element.phone}</span>
+              <span>
+                <img
+                  onclick={handleEdit}
+                  src="https://cdn-icons-png.flaticon.com/512/1160/1160758.png"
+                  alt="edit"
+                  style={{ maxWidth: "30px", cursor: "pointer" }}
+                />
+              </span>
+              <span>
+                <img
+                  onClick={() => handleDelete(element)}
+                  src="https://cdn-icons-png.flaticon.com/512/3177/3177433.png"
+                  alt="delete"
+                  style={{ maxWidth: "30px", cursor: "pointer" }}
+                />
+              </span>
+            </div>
+          ))}
         </div>
-        <div className="create-contact">
-          <CreateContact />
-        </div>
-        <div className="list-container">
-          <div className="main-container">
-            
-            {users.map((element) => (
-              <div className="container">
-                <span>{element.name}</span>
-                <span>{element.phone}</span>
-                <span>
-                  <div className="menu-nav">
-                    <div className="dropdown-container" tabindex="-1 ">
-                      <div className="three-dots"></div>
-                      <div className="dropdown">
-                        <div className="dropdown-list">
-                          <div className="Buttons">
-                          <div className="edit-button">
-                            <Button variant="primary" onClick={()=>this.setState({edit:true})}>Edit</Button>
-                          </div>
-
-                          <div>
-                            <Button variant="danger"
-                            onClick={()=>handleDelete(element)}>Delete</Button>
-                          </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </span>
-              </div>
-            ))}
-          </div>
-        </div>
-     
-          
-      </>
-    
-      
-    );
-  
+      </div>
+    </>
+  );
 }
 
 export default connect()(Home);
